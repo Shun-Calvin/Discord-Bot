4c655e1ca2bf5f29c2211c7e847c483cba14699d
Calvin_Shun <67963974+Shun-Calvin@users.noreply.github.com>
2023-09-07 10:47:03 +0800

    Update Translator.py

--- Translator.py
+++ Translator.py
@@ -1,12 +1,14 @@
+#Thanks to @1ntegrale9
+
 import translators as ts
+import translators.server as tss
 import discord
 from discord.ext import commands
 import nest_asyncio
 nest_asyncio.apply()
-ts.[translator] #select the translator
 
-intents = discord.Intents.all() 
-client = discord.Client()
+intents = discord.Intents.all()
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
@@ -15,47 +17,182 @@ async def on_ready():
     print('Ready to translate!')
 
 @client.event
-async def on_reaction_add(reaction, user): 
-    emojiid = reaction.emoji.encode('unicode-escape') #encode the emoji
+async def on_reaction_add(reaction, user):
+    print("emoji-id")
+    emojiid = reaction.emoji.encode('unicode-escape')
     print(emojiid)
-    if reaction.count == 1: #if there is new emoji after starting the bot
-      
+    if reaction.count == 1:
 #Japanese 
-        if emojiid == b'\\U0001f1ef\\U0001f1f5': #if reacted emoji is Japan 
+        if emojiid == b'\\U0001f1ef\\U0001f1f5':
+            tss.google
             phrase = reaction.message.content
-            trans_ja = ts.[translator](phrase, to_language='ja')
-            await reaction.message.channel.send(trans_ja)
+            trans_ja = tss.google(phrase, to_language='ja')
+            tran = discord.Embed(color = 0x2ecc71)
+            print(reaction.message.author.name)
+            tran.set_author(name = f'Translation Result ')
+            tran.add_field(name= f'Raw text from {reaction.message.author.name}', value = phrase, inline = False)
+            tran.add_field(name= f'Result  🇯🇵', value = trans_ja, inline = False)
+            tran.set_footer(text = f'Translator Bot From HolyShxxt Community')
+            await reaction.message.channel.send(embed = tran)
+            tran.remove_field(index=1)
+            tran.remove_field(index=0)
 
 #English
-        if emojiid == b'\\U0001f1fa\\U0001f1f8': #if reacted emoji is US
+        if emojiid == b'\\U0001f1fa\\U0001f1f8':
+            tss.google
             phrase = reaction.message.content
-            trans_en = ts.[translator](phrase, to_language='en')
-            await reaction.message.channel.send(trans_en)
+            trans_en = tss.google(phrase, to_language='en')
+            tran = discord.Embed(color = 0x2ecc71)
+            tran.set_author(name = f'Translation Result')
+            tran.add_field(name= f'Raw text from {reaction.message.author.name}', value = phrase, inline = False)
+            tran.add_field(name= f'Result  🇺🇸', value = trans_en, inline = False)
+            tran.set_footer(text = f'Translator Bot From HolyShxxt Community')
+            await reaction.message.channel.send(embed = tran)
+            tran.remove_field(index=1)
+            tran.remove_field(index=0)
 
 
-#Chinese
-        if emojiid == b'\\U0001f1ed\\U0001f1f0': #if reacted emoji is HK 
+#TraditionalChinese
+        if emojiid == b'\\U0001f1f9\\U0001f1fc':
+            tss.google
             phrase = reaction.message.content
-            trans_hk = ts.[translator](phrase, to_language='zh-TW')
-            await reaction.message.channel.send(trans_hk)
+            trans_hk = tss.google(phrase, to_language='zh')
+            tss.bing
+            trans_hk = tss.google(trans_hk, to_language='zh-TW')
+            tran = discord.Embed(color = 0x2ecc71)
+            tran.set_author(name = f'Translation Result ')
+            tran.add_field(name= f'Raw text from {reaction.message.author.name}', value = phrase, inline = False)
+            tran.add_field(name= f'Result 🇹🇼 ', value = trans_hk, inline = False)
+            tran.set_footer(text = f'Translator Bot From HolyShxxt Community')
+            await reaction.message.channel.send(embed = tran)
+            tran.remove_field(index=1)
+            tran.remove_field(index=0)
 
 #Korean
-        if emojiid == b'\\U0001f1f0\\U0001f1f7': #if reacted emoji is Korea 
+        if emojiid == b'\\U0001f1f0\\U0001f1f7':
             phrase = reaction.message.content
-            trans_ko = ts.[translator](phrase, to_language='ko')
-            await reaction.message.channel.send(trans_ko)
+            tss.google
+            trans_ko = tss.google(phrase, to_language='ko')
+            tran = discord.Embed(color = 0x2ecc71)
+            tran.set_author(name = f'Translation Result ')
+            tran.add_field(name= f'Raw', value = phrase, inline = False)
+            tran.add_field(name= f'Result 🇰🇷 ', value = trans_ko, inline = False)
+            tran.set_footer(text = f'Translator Bot From HolyShxxt Community')
+            await reaction.message.channel.send(embed = tran)
+            tran.remove_field(index=1)
+            tran.remove_field(index=0)
 
 #Spanish
-        if emojiid == b'\\U0001f1ea\\U0001f1f8': #if reacted emoji is Spain 
+        if emojiid == b'\\U0001f1ea\\U0001f1f8':
             phrase = reaction.message.content
-            trans_es = ts.[translator](phrase, to_language='es')
-            await reaction.message.channel.send(trans_es)
+            tss.google
+            trans_es = tss.google(phrase, to_language='es')
+            tran = discord.Embed(color = 0x2ecc71)
+            tran.set_author(name = f'Translation Result ')
+            tran.add_field(name= f'Raw text from {reaction.message.author.name}', value = phrase, inline = False)
+            tran.add_field(name= f'Result 🇪🇸 ', value = trans_es, inline = False)
+            tran.set_footer(text = f'Translator Bot From HolyShxxt Community')
+            await reaction.message.channel.send(embed = tran)
+            tran.remove_field(index=1)
+            tran.remove_field(index=0)
             
 #Portuguese 
-        if emojiid == b'\\U0001f1f5\\U0001f1f9': #if reacted emoji is Portugual  
+        if emojiid == b'\\U0001f1f5\\U0001f1f9':
             phrase = reaction.message.content
-            trans_pt = ts.[translator](phrase, to_language='pt')
-            await reaction.message.channel.send(trans_pt)
+            trans_pt = tss.google(phrase, to_language='pt')
+            tss.google
+            tran = discord.Embed(color = 0x2ecc71)
+            tran.set_author(name = f'Translation Result')
+            tran.add_field(name= f'Raw text from {reaction.message.author.name}', value = phrase, inline = False)
+            tran.add_field(name= f'Result  🇵🇹 ', value = trans_pt, inline = False)
+            tran.set_footer(text = f'Translator Bot From HolyShxxt Community')
+            await reaction.message.channel.send(embed = tran)
+            tran.remove_field(index=1)
+            tran.remove_field(index=0)
 
-client.run([Token])
+#SimplifiedChinese 
+        if emojiid == b'\\U0001f1e8\\U0001f1f3':
+            phrase = reaction.message.content
+            tss.google
+            trans_zh = tss.google(phrase, to_language='zh')
+            tran = discord.Embed(color = 0x2ecc71)
+            tran.set_author(name = f'Translation Result')
+            tran.add_field(name= f'Raw text from {reaction.message.author.name}', value = phrase, inline = False)
+            tran.add_field(name= f'Result  🇨🇳', value = trans_zh, inline = False)
+            tran.set_footer(text = f'Translator Bot From HolyShxxt Community')
+            await reaction.message.channel.send(embed = tran)
+            tran.remove_field(index=1)
+            tran.remove_field(index=0)
 
+#Cantonese 
+        if emojiid == b'\\U0001f1ed\\U0001f1f0':
+            phrase = reaction.message.content
+            tss.bing
+            trans = tss.bing(phrase, to_language='yue')
+            trans_ca = tss.google(trans, to_language='zh-TW')
+            tran = discord.Embed(color = 0x2ecc71)
+            tran.set_author(name = f'Translation Result')
+            tran.add_field(name= f'Raw text from {reaction.message.author.name}', value = phrase, inline = False)
+            tran.add_field(name= f'Result  🇭🇰', value = trans_ca, inline = False)
+            tran.set_footer(text = f'Translator Bot From HolyShxxt Community')
+            await reaction.message.channel.send(embed = tran)
+            tran.remove_field(index=1)
+            tran.remove_field(index=0)
+
+#CantoneseToEnglish
+        if emojiid == b'\\U0001f1ec\\U0001f1e7':
+            phrase = reaction.message.content
+            tss.baidu
+            trans_gb = tss.bing(phrase, from_language='yue', to_language='en')
+            tran = discord.Embed(color = 0x2ecc71)
+            tran.set_author(name = f'Translation Result')
+            tran.add_field(name= f'Raw text from {reaction.message.author.name}', value = phrase, inline = False)
+            tran.add_field(name= f'Result  🇬🇧', value = trans_gb, inline = False)
+            tran.set_footer(text = f'Translator Bot From HolyShxxt Community')
+            await reaction.message.channel.send(embed = tran)
+            tran.remove_field(index=1)
+            tran.remove_field(index=0)
+
+#Dutch
+        if emojiid == b'\\U0001f1f3\\U0001f1f1':
+            phrase = reaction.message.content
+            tss.google
+            trans_nl = tss.google(phrase, to_language='nl')
+            tran = discord.Embed(color = 0x2ecc71)
+            tran.set_author(name = f'Translation Result')
+            tran.add_field(name= f'Raw text from {reaction.message.author.name}', value = phrase, inline = False)
+            tran.add_field(name= f'Result  🇳🇱', value = trans_nl, inline = False)
+            tran.set_footer(text = f'Translator Bot From HolyShxxt Community')
+            await reaction.message.channel.send(embed = tran)
+            tran.remove_field(index=1)
+            tran.remove_field(index=0)
+
+#Russian
+        if emojiid == b'\\U0001f1f7\\U0001f1fa':
+            phrase = reaction.message.content
+            tss.google
+            trans_ru = tss.google(phrase, to_language='ru')
+            tran = discord.Embed(color = 0x2ecc71)
+            tran.set_author(name = f'Translation Result')
+            tran.add_field(name= f'Raw text from {reaction.message.author.name}', value = phrase, inline = False)
+            tran.add_field(name= f'Result  🇷🇺', value = trans_ru, inline = False)
+            tran.set_footer(text = f'Translator Bot From HolyShxxt Community')
+            await reaction.message.channel.send(embed = tran)
+            tran.remove_field(index=1)
+            tran.remove_field(index=0)
+
+#French
+        if emojiid == b'\\U0001f1eb\\U0001f1f7':
+            phrase = reaction.message.content
+            tss.google
+            trans_fr = tss.google(phrase, to_language='fr')
+            tran = discord.Embed(color = 0x2ecc71)
+            tran.set_author(name = f'Translation Result')
+            tran.add_field(name= f'Raw text from {reaction.message.author.name}', value = phrase, inline = False)
+            tran.add_field(name= f'Result 🇫🇷', value = trans_fr,inline = False)
+            tran.set_footer(text = f'Translator Bot From HolyShxxt Community')
+            await reaction.message.channel.send(embed = tran)
+            tran.remove_field(index=1)
+            tran.remove_field(index=0)
+
+client.run("MTAzMjUzNzk3NjMzNDQ2MzAzNg.Ghpe_q.CIwyv41_NOOxqIWCbY8xBeIqr17dVKHkbkzVPk")
